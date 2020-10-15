a7be3f64471ee9c981d3f4802e7127b36ed6b9c3
tinycoding5 <tinycoding5@gmail.com>
2020-10-15 19:26:24 +0800

    first commit

---
+++ index.android.js
@@ -0,0 +1,22 @@
+/**
+ * @providesModule JitsiMeet
+ */
+
+import { NativeModules, requireNativeComponent } from 'react-native';
+
+export const JitsiMeetView = requireNativeComponent('RNJitsiMeetView');
+export const JitsiMeetModule = NativeModules.RNJitsiMeetModule
+const call = JitsiMeetModule.call;
+const audioCall = JitsiMeetModule.audioCall;
+JitsiMeetModule.call = (url, userInfo, callSettings) => {
+  userInfo = userInfo || {};
+  callSettings = callSettings || {};
+  call(url, userInfo, callSettings);
+}
+JitsiMeetModule.audioCall = (url, userInfo) => {
+  userInfo = userInfo || {};
+  audioCall(url, userInfo);
+}
+export default JitsiMeetModule;
+
+
